53bb91b30cf95a905972c2b3adb551aeb4c5b906
fbensibpsolutions <f.bensi@bpsolutions.biz>
2021-09-22 08:27:19 +0800

    Add static data for order.

--- BPS0026_MVC_WEB/BPS0026_MVC_WEB/Controllers/HomeController.cs
+++ BPS0026_MVC_WEB/BPS0026_MVC_WEB/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BPS0026_MVC_WEB.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,8 +11,71 @@ namespace BPS0026_MVC_WEB.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            List<OrderModel> orderList = new List<OrderModel>();
+
+            orderList = OrderDetailsData();
+
+            return View(orderList);
         }
+
+        public ActionResult Orderdetails(string no)
+        {
+            ViewData["no"] = no;
+
+            var data = OrderDetailsData().FirstOrDefault(x => x.No == no);
+
+            var orderdetailsdatas = new List<OrderModel>();
+
+            orderdetailsdatas.Add(new OrderModel
+            {
+                No = data.No,
+                CustomerName = data.CustomerName,
+                Date = data.Date,
+                Amount = data.Amount
+            });
+
+            return View(orderdetailsdatas);
+        }
+
+
+        public List<OrderModel> OrderDetailsData()
+        {
+            var orders = new List<OrderModel>();
+
+            orders.Add(new OrderModel
+            {
+                No = "OD-1",
+                CustomerName = "Test-1",
+                Date = DateTime.Now.ToString("MM/dd/yyyy"),
+                Amount = 1000
+            });
+            orders.Add(new OrderModel
+            {
+                No = "OD-2",
+                CustomerName = "Test-2",
+                Date = DateTime.Now.ToString("MM/dd/yyyy"),
+                Amount = 1050
+            });
+
+            orders.Add(new OrderModel
+            {
+                No = "OD-3",
+                CustomerName = "Test-3",
+                Date = DateTime.Now.ToString("MM/dd/yyyy"),
+                Amount = 1050
+            });
+
+            orders.Add(new OrderModel
+            {
+                No = "OD-4",
+                CustomerName = "Test-4",
+                Date = DateTime.Now.ToString("MM/dd/yyyy"),
+                Amount = 1050
+            });
+
+            return orders;
+        }
+
 
         public ActionResult About()
         {
---
+++ BPS0026_MVC_WEB/BPS0026_MVC_WEB/Models/OrderModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BPS0026_MVC_WEB.Models
+{
+    public class OrderModel
+    {
+            public string No { get; set; }
+            public string Date { get; set; }
+            public string CustomerName { get; set; }
+            public int Amount { get; set; }
+    }
+}
