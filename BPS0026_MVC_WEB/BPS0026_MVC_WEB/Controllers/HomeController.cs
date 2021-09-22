b9baeb752924649cf6f81ad59369ac517dc8c51d
fbensibpsolutions <f.bensi@bpsolutions.biz>
2021-09-23 07:43:34 +0800

    Add static data for order.

--- BPS0026_MVC_WEB/BPS0026_MVC_WEB/Controllers/HomeController.cs
+++ BPS0026_MVC_WEB/BPS0026_MVC_WEB/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BPS0026_MVC_WEB.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,21 +11,69 @@ namespace BPS0026_MVC_WEB.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            List<Order> orderList = new List<Order>();
+
+            orderList = OrderDetailsData();
+
+            return View(orderList);
         }
 
-        public ActionResult About()
+        public ActionResult Orderdetails(string no)
         {
-            ViewBag.Message = "Your application description page.";
+            ViewData["no"] = no;
 
-            return View();
+            var data = OrderDetailsData().FirstOrDefault(x => x.No == no);
+
+            var orderdetailsdatas = new List<Order>();
+
+            orderdetailsdatas.Add(new Order
+            {
+                No = data.No,
+                CustomerName = data.CustomerName,
+                Date = data.Date,
+                Amount = data.Amount
+            });
+
+            return View(orderdetailsdatas);
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
 
-            return View();
+        public List<Order> OrderDetailsData()
+        {
+            var orders = new List<Order>();
+
+            orders.Add(new Order
+            {
+                No = "OD-1",
+                CustomerName = "Test-1",
+                Date = DateTime.Now.ToString("MM/dd/yyyy"),
+                Amount = 1000
+            });
+            orders.Add(new Order
+            {
+                No = "OD-2",
+                CustomerName = "Test-2",
+                Date = DateTime.Now.ToString("MM/dd/yyyy"),
+                Amount = 1050
+            });
+
+            orders.Add(new Order
+            {
+                No = "OD-3",
+                CustomerName = "Test-3",
+                Date = DateTime.Now.ToString("MM/dd/yyyy"),
+                Amount = 1050
+            });
+
+            orders.Add(new Order
+            {
+                No = "OD-4",
+                CustomerName = "Test-4",
+                Date = DateTime.Now.ToString("MM/dd/yyyy"),
+                Amount = 1050
+            });
+
+            return orders;
         }
     }
 }
